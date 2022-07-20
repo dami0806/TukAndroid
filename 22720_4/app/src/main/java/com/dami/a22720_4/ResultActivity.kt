167d8bdb78a2f33463db5257744185f08e911ce4
dami <dami0804@naver.com>
2022-07-20 16:46:45 +0900

    vote pictures

---
+++ 22720_4/app/src/main/java/com/dami/a22720_4/ResultActivity.kt
@@ -0,0 +1,62 @@
+package com.dami.a22720_4
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        title = "투표 결과"
+
+        // 앞 화면에서 보낸 투표 결과 값을 받는다.
+        var intent = intent
+        var voteResult = intent.getIntArrayExtra("VoteCount")
+        var imageName = intent.getStringArrayExtra("ImageName")
+
+
+        // 9개의 TextView, RatingBar 객체배열
+        var tv = arrayOfNulls<TextView>(imageName!!.size)
+        var rbar = arrayOfNulls<RatingBar>(imageName.size)
+
+        // 9개의 TextView, RatingBar ID 배열
+        var tvID = arrayOf(R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5, R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9)
+
+        var rbarID = arrayOf(R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4, R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9)
+        val imageFileId = arrayOf(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4,
+            R.drawable.pic5, R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9)
+        val ivtext = findViewById<TextView>(R.id.tvTop)
+        val ivimage = findViewById<ImageView>(R.id.ivTop)
+        var maxEntry=0
+        for (i in 1 until voteResult!!.size) {
+            if (voteResult!![maxEntry] < voteResult!![i]) maxEntry = i
+        }
+        ivtext.setText(imageName[maxEntry])
+        ivimage.setImageResource(imageFileId[maxEntry])
+        // TextView, RatingBar 개체 찾기.
+        for (i in voteResult!!.indices) {
+            tv[i] = findViewById<TextView>(tvID[i])
+            rbar[i] = findViewById<RatingBar>(rbarID[i])
+        }
+
+        // 각 TextVeiw 및 RatingBar에 넘겨 받은 값을 반영.
+        for (i in voteResult.indices) {
+            tv[i]!!.setText(imageName[i])
+            rbar[i]!!.setRating(voteResult[i].toFloat())
+
+
+        }
+
+        var btnReturn = findViewById<Button>(R.id.btnReturn)
+        btnReturn.setOnClickListener {
+            finish()
+        }
+
+    }
+}
